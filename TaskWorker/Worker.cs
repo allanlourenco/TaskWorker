19fb51e4ec15d41057a7443ebcd1946deaa0622f
allan.lourenco <allan.lourenco@outlook.com>
2024-07-22 09:47:35 -0300

    Adicionar arquivos de projeto.

---
+++ TaskWorker/Worker.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> _logger;
+    private IConnection _connection;
+    private IModel _channel;
+
+    public Worker(ILogger<Worker> logger)
+    {
+        _logger = logger;
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        _connection = factory.CreateConnection();
+        _channel = _connection.CreateModel();
+        _channel.QueueDeclare(queue: "taskQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var consumer = new EventingBasicConsumer(_channel);
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            _logger.LogInformation($"Received message: {message}");
+            // Process the message
+        };
+        _channel.BasicConsume(queue: "taskQueue", autoAck: true, consumer: consumer);
+
+        await Task.CompletedTask;
+    }
+
+    public override void Dispose()
+    {
+        _channel.Close();
+        _connection.Close();
+        base.Dispose();
+    }
+}
